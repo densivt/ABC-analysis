450577c9432605b995894a1332315868c624d7b4
densivt <denissivtsev@yandex.ru>
2016-05-21 21:31:44 +0300

    alpha_version

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <QDebug>
 #include <ctime>
 #include <limits>
+#include <cstdlib>
 
 using namespace std;
 
@@ -24,30 +25,51 @@ QMap<QString, QString> getInfo(QString fileName){
     return map;
 }
 
-void toFile(QList<QString> id, QList<double> value){
-    QFile file("out.txt");
+void toFile(QString fileName, QList<QString> id, QList<QString> letter){
+    QFile file(fileName);
     file.open(QIODevice::WriteOnly | QIODevice::Text);
     QTextStream out(&file);
     for(int i=0; i<id.size(); i++){
-        out << id.at(i) << "\t" << value.at(i) << "\n";
+        out << id.at(i) << "\t" << letter.at(i) << "\n";
     }
     return;
 }
 
-int main(/*int argc, char *argv[]*/)
+int main(int argc, char *argv[])
 {
     //QCoreApplication a(argc, argv);
     //return a.exec();
+    QString INPUT_FILE_NAME;
+    QString OUTPUT_FILE_NAME;
+    int POWER;
+    int AB;
+    int BC;
+    QList<QString> id;
+    QList<QString> letter;
+    if (argc < 5){
+        qDebug() << "ERROR! WRONG PARAMETERS!";
+        qDebug() << "Press ANY key.";
+        return 0;
+    } else {
+        INPUT_FILE_NAME = argv[1];
+        OUTPUT_FILE_NAME = argv[2];
+        POWER = atoi(argv[3]);
+        AB = atoi(argv[4]);
+        BC = atoi(argv[5]);
+    }
     int start_time = clock();
-    QString INPUT_FILE_NAME = "input.txt";
-    QFile file(INPUT_FILE_NAME);
+    qDebug() << "Starting...";
     double SUMM = 0;
+    qDebug() << "Reading from file...";
+    QFile file(INPUT_FILE_NAME);
     if(!file.exists())
     {
-        qDebug() << "Where is my f#$kin' file!";
+        qDebug() << "ERROR! NO INPUT FILE!";
+        qDebug() << "Press ANY key.";
+        return 0;
     }
     QMap<QString, QString> map = getInfo(INPUT_FILE_NAME);
-    QList<QString> id;
+    qDebug() << "Calculating...";
     QList<double> value;
     id << "toDel";
     value << 0.0;
@@ -66,9 +88,21 @@ int main(/*int argc, char *argv[]*/)
     for (int i=1; i < value.size(); i++){
         value[i] = (value[i]/SUMM)*100 + value[i-1];
     }
-    toFile(id, value);
-    qDebug() << SUMM;
+    id.removeLast();
+    value.removeLast();
+    for(int i=0; i<value.size(); i++){
+        if (value.at(i)<AB){
+            letter << "A";
+        } else if (value.at(i)<BC){
+            letter << "B";
+        } else {
+            letter << "C";
+        }
+    }
+    qDebug() << "Writing to file...";
+    toFile(OUTPUT_FILE_NAME, id, letter);
     int finish_time = clock();
-    qDebug() << (finish_time - start_time) / 1000.0;
+    qDebug() << "Performed for: " << (finish_time - start_time) / 1000.0 <<"s.";
+    qDebug() << "Press ANY key.";
     return 0;
 }
